72b16caedd1d95e4e7e324a90dd40ba9f5d784b9
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-03-27 15:54:51 -0300

    feat: alterações no post, get, delete

---
+++ src/routes/notasRoutes.js
@@ -0,0 +1,10 @@
+
+import express from "express";
+import notasController from "../controllers/notasController.js";
+ const router = express.Router();
+ router.get("/", notasController.getAll);
+ router.post("/", notasController.create);
+ router.put("/:id", notasController.update);
+ router.delete("/:id", notasController.delete);
+
+ export default router;
