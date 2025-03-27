96f884aff82a8b84d0ccfed96c31f924e21bcbc8
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-03-27 16:16:31 -0300

    feat: alterações no readme.md e no update

---
+++ README.md
@@ -0,0 +1,25 @@
+# QuickNotes - API de Anotações
+
+Este projeto é uma API RESTful para gerenciar anotações. Ela permite criar, listar, atualizar, excluir e marcar anotações como favoritas. A API foi desenvolvida usando **Node.js**.
+
+## Funcionalidades
+
+- Criar novas anotações
+- Listar todas as anotações
+- Buscar anotações por ID
+- Atualizar anotações
+- Deletar anotações
+- Marcar anotações como favoritas
+
+## Tecnologias Utilizadas
+
+- **Node.js**: Framework backend para JavaScript.
+- **Express**: Framework para construção da API.
+- **Prisma**
+
+## Pré-requisitos
+
+ Itens instalados na minha máquina:
+
+- **Node.js**
+- **Banco de dados SQLite** 
--- src/models/notasModel.js
+++ src/models/notasModel.js
@@ -1,44 +1,47 @@
 import prisma from "../../prisma/client.js";
- 
- class notasModel {
-   getAll = async () => {
-     return await prisma.nota.findMany();
-   }
- 
-   create = async (titulo, conteudo) => {
-     return await prisma.nota.create({
-       data: {
-         titulo: titulo,
-         conteudo: conteudo,
-       },
-     })
-   };
- 
-   update = async (id, titulo) => {
-     try {
-       const tarefa = await prisma.nota.update({
-         where: { id },
-         data: {
-           titulo: titulo !== undefined ? titulo : true,
-         }
-       });
-       return tarefa;
-     } catch (error) {
-       console.log("Error", error);
-       throw error
-     }
-   };
- 
-   delete = async (id) => {
-     try {
-       const tarefaDeletada = await prisma.nota.delete({
-         where: { id },
-       });
-       return tarefaDeletada;
-     } catch (error) {
-       console.error("Error ao deletar a nota!", error);
-       throw error;
-     }
-   };
- }
- export default new notasModel();
+
+class notasModel {
+  getAll = async () => {
+    return await prisma.nota.findMany();
+  }
+
+  create = async (titulo, conteudo) => {
+    return await prisma.nota.create({
+      data: {
+        titulo: titulo,
+        conteudo: conteudo,
+      },
+    })
+  };
+
+  update = async (id, titulo, conteudo, favorito, cor) => {
+    try {
+      const nota = await prisma.nota.update({
+        where: { id },
+        data: {
+          titulo,
+          conteudo,
+          favorito,
+          cor,
+        }
+      });
+      return nota;
+    } catch (error) {
+      console.log("Error", error);
+      throw error
+    }
+  };
+
+  delete = async (id) => {
+    try {
+      const notaDeletada = await prisma.nota.delete({
+        where: { id },
+      });
+      return notaDeletada;
+    } catch (error) {
+      console.error("Error ao deletar a nota!", error);
+      throw error;
+    }
+  };
+}
+export default new notasModel();
